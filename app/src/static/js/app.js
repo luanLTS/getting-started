f22da63aa95db4d7f662eb317ff598ede93df19e
Luan Teixeira <luanlts@outlook.com>
2022-12-29 08:20:31 -0300

    feat: altera titulo quando nao ha tarefas na lista e faz rebuild da imagem de container

--- app/src/static/js/app.js
+++ app/src/static/js/app.js
@@ -53,7 +53,7 @@ function TodoListCard() {
         <React.Fragment>
             <AddItemForm onNewItem={onNewItem} />
             {items.length === 0 && (
-                <p className="text-center">No items yet! Add one above!</p>
+                <p className="text-center">Não tem nenhum item, que tal adicionar um agora?</p>
             )}
             {items.map(item => (
                 <ItemDisplay
